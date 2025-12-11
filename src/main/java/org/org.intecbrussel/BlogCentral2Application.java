9e702c2b785d529ecafb709f54c26c2273a873aa
3 Klasses  Update <hajji.chaima@outlook.fr>
2025-12-11 10:22:49 +0100

    update

---
+++ src/main/java/org/org.intecbrussel/BlogCentral2Application.java
@@ -0,0 +1,13 @@
+package org.intecbrussel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCentral2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogCentral2Application.class, args);
+	}
+
+}
